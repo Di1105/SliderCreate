f3b838b96f10960f81a90ca03921ad3ac9a076e3
Di1105 <delcioglu@gmail.com.com>
2022-12-23 17:14:56 +0300

    Initial Commit

---
+++ SliderCreate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderCreate
+//
+//  Created by Dilara Elçioğlu on 23.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
